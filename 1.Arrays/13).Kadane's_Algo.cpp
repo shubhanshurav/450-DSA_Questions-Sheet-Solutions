1c1958c27bdda59a5527386b8be9319caedaec2e
Shubhanshu Rao <87806305+shubhanshurav@users.noreply.github.com>
2022-12-30 20:05:25 +0530

    Update repo

---
+++ 1.Arrays/13).Kadane's_Algo.cpp
@@ -0,0 +1,25 @@
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int a[], int n){
+        
+        int currentSum = 0;
+        int maxSum = INT_MIN;
+        
+        for(int i=0; i<n; i++){
+            
+            currentSum = currentSum+a[i];
+            maxSum = max(currentSum,maxSum);
+            
+            if(currentSum < 0){
+                currentSum = 0;
+            }
+            
+        }
+        
+        return maxSum;
+        
+    }
+};
