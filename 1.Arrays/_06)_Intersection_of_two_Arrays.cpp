26df3e137fe30c728386a0e2437f4975ba7d8cc3
Shubhanshu Rao <87806305+shubhanshurav@users.noreply.github.com>
2022-12-30 21:31:03 +0530

    Update repo

--- 1.Arrays/_06)_Intersection_of_two_Arrays.cpp
+++ 1.Arrays/_06)_Intersection_of_two_Arrays.cpp
@@ -1,3 +1,5 @@
+//<----> Leetcode Solution <------------->
+
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
@@ -27,3 +29,65 @@ public:
     }
      
 };
+
+
+//<-----------> GFG Question Solutions <-------------->
+ 
+class Solution {
+  public:
+    // Function to return the count of the number of elements in
+    // the intersection of two arrays.
+    int NumberofElementsInIntersection(int a[], int b[], int n, int m) {
+        int count = 0,i=0,j=0;
+        
+        sort(a,a+n);  //sort the array a[]
+        sort(b,b+m);  //sort the array b[]
+        
+        // i will go upto size n & j will go upto size m
+        while(i<n && j<m){
+            // this condition will handle the duplicate from the array
+            if(i>0 && a[i] == a[i-1]){
+              i++;
+              continue;
+            } 
+            
+            //if array a[] element is smaller than array b[] than we will increment the i++
+            if(a[i]<b[j]){
+                i++;
+            }else if(a[i]>b[j]){   //if array b[] element is smaller than array a[] than we will increment the j++
+                j++;
+            }else{                 //if array a[] element is equal to the array b[] than count ++ & we will increment the i++,j++
+                count++;
+                i++;
+                j++;
+            }
+        }
+        
+        return count;
+    }
+};
+
+
+/*
+
+class Solution {
+  public:
+    // Function to return the count of the number of elements in
+    // the intersection of two arrays.
+    int NumberofElementsInIntersection(int a[], int b[], int n, int m) {
+        int count = 0;
+        for(int i=0; i<n;i++){
+           if(i>0 && a[i] == a[i-1]) continue;
+            
+           for(int j=0; j<m; j++){
+              if(a[i] == b[j]){
+                  count++;
+                  break;
+              } 
+           }
+        }    
+        return count;
+    }
+};
+
+*/
