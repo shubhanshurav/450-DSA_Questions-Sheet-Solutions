671e1d3e06289c8e4d99b92de128b0be8cce4976
Shubhanshu Rao <87806305+shubhanshurav@users.noreply.github.com>
2022-12-31 10:34:53 +0530

    Update repo

--- 3.Strings/1..Reverse_a_String.cpp
+++ 3.Strings/1..Reverse_a_String.cpp
@@ -1,10 +1,23 @@
+//<----------------> GFG Question Solution <--------------->
+
 //User function Template for C++
 string reverseWord(string str){
-    
-  //Your code here
+
   int n = str.size();
   
   for(int i=n; i>=0; i--){
       cout<<str[i];
   }
 }
+
+
+
+
+//<----------------> Leetcode Question Solution <--------------->
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+       reverse(s.begin(), s.end());  
+    }
+};
