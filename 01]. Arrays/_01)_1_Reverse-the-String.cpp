48d7a1a0fa7fc660699b6d4a511924d282ecc228
Shubhanshu Rao <87806305+shubhanshurav@users.noreply.github.com>
2023-01-29 17:52:14 +0530

    Update repo

--- 01]. Arrays/_01)_1_Reverse-the-String.cpp
+++ 01]. Arrays/_01)_1_Reverse-the-String.cpp
@@ -11,23 +11,22 @@ Example 2:
 using namespace std;
 
 string reverseWord(string str){
-  //Your code here
+  
   int low=0, high=str.length()-1;
   
-  while(low < high)
-  {
-      swap(str[low], str[high]);
-      low++;
-      high--;
-  }
+    while(low < high){
+	 swap(str[low], str[high]);
+	 low++;
+	 high--;
+    }
+	
   return str;
 }
 
 int main() {
     int t;
     cin>>t;
-    while(t--)
-    {
+    while(t--){
 	string s;
         cin >> s;
 	cout << reverseWord(s) << endl;
