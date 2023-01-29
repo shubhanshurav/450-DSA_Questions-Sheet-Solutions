17e20f14afa15e73d5189364607fd9f5da8690f8
Shubhanshu Rao <87806305+shubhanshurav@users.noreply.github.com>
2023-01-29 17:55:41 +0530

    Update repo

--- 02]. Matrix/2.1). Search_a_2D_Matrix.cpp
+++ 02]. Matrix/2.1). Search_a_2D_Matrix.cpp
@@ -10,10 +10,12 @@
 // Output: true
 
 //Using binary_search STL
+
 bool searchMatrix(vector<vector<int>>& matrix, int target) {
         
         for(auto i : matrix) 
-            if(binary_search(i.begin(),i.end(),target))
-               return true;
+                
+            if(binary_search(i.begin(),i.end(),target))  return true;
+        
             return false;
 }
