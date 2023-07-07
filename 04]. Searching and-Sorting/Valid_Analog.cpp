8d88f17b0f2e5c5a15e1cdc22c705628e822182e
shubhanshurav <shubhanshurao7037@gmail.com>
2023-07-08 00:15:17 +0530

    Add Questions

---
+++ 04]. Searching and-Sorting/Valid_Analog.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+          
+        //sort the both strings  
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+
+        if(s.size() == t.size()){
+            for(int i=0;i<s.size();i++){
+                if(s[i] == t[i]){
+                    continue;
+                }else{
+                    return false;
+                }
+            }
+        }else{
+            return false;
+        }
+
+    return true;
+    }
+};
